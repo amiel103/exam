28808e0f87992e617e334f4ed01683c386f6fea3
Jezzel Faith Gier <156341022+Jzelfthr@users.noreply.github.com>
2025-03-28 17:05:08 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
 import csv
 import os
 app = FastAPI()
