586309f944a283ede00aa6d19aadea8941178a84
Airyll Sanchez <sanchezaeri29@gmail.com>
2025-05-27 15:19:52 +0800

    Complete merge: resolved conflicts in main.py

--- main.py
+++ main.py
@@ -138,7 +138,7 @@ async def get_tasks(name: str):
 
     return {"tasks": [ ['laba','2','a'] , ['study','6','a'] , ['code','10','a']  ] }
 
-@app.post("/complete_task/") #sanchezairyll
+@app.post("/complete_task/") #sanchezairyllds3a
 async def complete_task(data: TaskCompletion):
     """
     Marks a specific task as completed or not completed.
